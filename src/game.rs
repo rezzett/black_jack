bc5d10e71c971f84d6cc31cadd23efd9f00b0cf7
rezzett <rezzett@mail.ru>
2020-06-28 18:22:56 +0600

    add all commit

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,6 @@
 use crate::player::Player;
 use std::io::stdin;
+use rand::{thread_rng, Rng};
 
 pub fn run() {
     println!("\n\n{0} {3} {2} {1} {3} {0}    Black Jack!    {1} {2} {0} {3} {1} {0}\n",
@@ -8,53 +9,62 @@ pub fn run() {
     let player = input("Enter your name:");
     let mut player = Player::new(&player);
     let mut dealer = Player::new("Dealer");
-    let mut deck = Vec::new();
     let mut player_pass = false;
     let mut dealer_pass = false;
-    for i in 2..15 {
-        deck.push(i);
-        deck.push(i);
-        deck.push(i);
-        deck.push(i);
-    }
 
     loop {
-        if dealer.score > 17 {
-            dealer_pass = true;
-        }
-        if !dealer_pass {
-            dealer.take_card(&mut deck);
-            println!("Диллер берет карту.");
-            if dealer.is_overflow() {
-                print_result(&dealer, &player, "\u{2665} У диллера преребор! Вы виграли! \u{2663}");
+        let mut deck = make_deck();
+        let bot_decision = bot_decide_stop();
+        loop {
+            if dealer.score > bot_decision {
+                dealer_pass = true;
+                println!("The dealer saved");
+            }
+            if !dealer_pass {
+                dealer.take_card(&mut deck);
+                println!("The dealer takes a card");
+                if dealer.is_overflow() {
+                    print_result(&dealer, &player, "\u{2665} Overkill! You win! \u{2663}");
+                    break;
+                }
+            }
+
+            if !player_pass {
+                let decision = input("Do you want take one more card?(y/n)");
+                if decision.as_str() == "y" {
+                    let card = player.take_card(&mut deck);
+                    println!("You got a card: [{}]. Your score: {}", pretty_card(card), player.score);
+
+                    if player.is_overflow() {
+                        print_result(&dealer, &player, "\u{2666} Overkill! You lose! \u{2664}");
+                        break;
+                    }
+                } else {
+                    player_pass = true;
+                    println!("{} saved.", player.name);
+                }
+            }
+
+            if dealer_pass && player_pass {
+                if dealer.score > player.score {
+                    print_result(&dealer, &player, "\u{2666} You lose! \u{2665}");
+                } else if dealer.score < player.score {
+                    print_result(&dealer, &player, "\u{2663} You win! \u{2664}");
+                } else {
+                    print_result(&dealer, &player, "\u{2665} Draw! \u{2665}");
+                }
                 break;
             }
         }
 
-        if !player_pass {
-            let decision = input("Вы хотите взять еще одну карту?(y/n)");
-            if decision.as_str() == "y" {
-                let card = player.take_card(&mut deck);
-                println!("Вам пришла карта {}. всего очков: {}", pretty_card(card), player.score);
-
-                if player.is_overflow() {
-                    print_result(&dealer, &player, "\u{2666} У Вас преребор! Вы проиграли! \u{2664}");
-                    break;
-                }
-            } else {
-                player_pass = true;
-                println!("{} спасовал.", player.name);
-            }
-        }
-
-        if dealer_pass && player_pass {
-            if dealer.score > player.score {
-                print_result(&dealer, &player, "\u{2666} Вы проиграли! \u{2665}");
-            } else if dealer.score < player.score {
-                print_result(&dealer, &player, "\u{2663} Вы Выиграли! \u{2664}");
-            } else {
-                print_result(&dealer, &player, "\u{2665} Ничья! \u{2665}");
-            }
+        if do_play_again() {
+            player.score = 0;
+            dealer.score = 0;
+            player_pass = false;
+            dealer_pass = false;
+            continue;
+        } else {
+            println!("Thank you! Good bye!");
             break;
         }
     }
@@ -71,21 +81,45 @@ fn print_result(player: &Player, dealer: &Player, message: &str) {
     println!("{}\n {}-{}\n {}-{}", message, dealer.name, dealer.score, player.name, player.score);
 }
 
+fn do_play_again() -> bool {
+    let decision = input("Do you want play again?(y/n)");
+    if &decision == "y" {
+        true
+    } else {
+        false
+    }
+}
+
+fn make_deck() -> Vec<usize> {
+    let mut deck = Vec::new();
+    for i in 2..15 {
+        deck.push(i);
+        deck.push(i);
+        deck.push(i);
+        deck.push(i);
+    }
+    deck
+}
+
+fn bot_decide_stop() -> usize {
+    thread_rng().gen_range(14, 19)
+}
+
 fn pretty_card(n: usize) -> String {
     match n {
-        2 => String::from("Двойка"),
-        3 => String::from("Тройка"),
-        4 => String::from("Четверка"),
-        5 => String::from("Пятерка"),
-        6 => String::from("Шестерка"),
-        7 => String::from("Семерка"),
-        8 => String::from("Восьмерка"),
-        9 => String::from("Девятка"),
-        10 => String::from("Десятка"),
-        11 => String::from("Валет"),
-        12 => String::from("Дама"),
-        13 => String::from("Король"),
-        14 => String::from("Туз"),
+        2 => String::from("Two"),
+        3 => String::from("Three"),
+        4 => String::from("Four"),
+        5 => String::from("Five"),
+        6 => String::from("Six"),
+        7 => String::from("Seven"),
+        8 => String::from("Eight"),
+        9 => String::from("Nine"),
+        10 => String::from("Ten"),
+        11 => String::from("Jack"),
+        12 => String::from("Queen"),
+        13 => String::from("King"),
+        14 => String::from("Ace"),
         _ => String::new(),
     }
 }
